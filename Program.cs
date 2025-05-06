9a47ff3695dd7d86ff0cf4bff8494b8d324590d6
Kavarn Francis (1000070701) <FRANKM2@student.op.ac.nz>
2025-05-06 13:36:56 +1200

    Kavarns Code

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@
             Console.WriteLine("1");
             Console.WriteLine("1");
 
-            Console.WriteLine("2");
-            Console.WriteLine("2");
-            Console.WriteLine("2");
-            Console.WriteLine("2");
-            Console.WriteLine("2");
+            Console.WriteLine("My Name is Kavarn");
+            Console.WriteLine("I am 24");
+            Console.WriteLine("I don't");
+            Console.WriteLine("know what else to");
+            Console.WriteLine("write");
 
             Console.WriteLine("3");
             Console.WriteLine("3");
