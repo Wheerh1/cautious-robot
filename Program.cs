5c5772c916ba17b35835136ee7d2d9b30e3c7189
Kavarn Francis (1000070701) <FRANKM2@student.op.ac.nz>
2025-05-06 13:35:26 +1200

    1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1");
+            Console.WriteLine("1");
+            Console.WriteLine("1");
+            Console.WriteLine("1");
+            Console.WriteLine("1");
+
+            Console.WriteLine("2");
+            Console.WriteLine("2");
+            Console.WriteLine("2");
+            Console.WriteLine("2");
+            Console.WriteLine("2");
+
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+
         }
     }
 }
